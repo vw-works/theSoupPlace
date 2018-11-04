1c456d5ded959614a297c6430d7fea91a5d1b93f
vw-works <vila.wong.works@gmail.com>
2018-11-04 07:47:52 -0800

    converted footer to php file

---
+++ Application/includes/footer.php
@@ -0,0 +1,58 @@
+<!-- END CHANGEABLE CONTENT -->
+</main>
+      <footer>
+        <div class="container py-5">
+          <div class="row justify-content-between">
+            <!-- SITE MAP SECTION -->
+            <div class="col-md-3">
+              <span>
+                  (011) 4964-2888</br>
+                  995 Nosoupforelaine Bvld</br>
+                  Recoleta Buenos Aires</br>
+                  Argentina</br>
+              </span>
+            </div>
+            <!-- CONTACT INFORMATION -->
+            <div class="col-md-3">
+              <ul>
+                  <li><a href="index.html#procedure">Procedure &amp; Rules</a></li>
+                  <li><a href="#">Menu</a></li>
+                  <li><a href="#">Hours &amp; Location</a></li>
+              </ul>
+                
+            </div>
+          </div>
+          <!-- Disclaimer -->
+          <div class="row mt-4 copyright">
+            <div class="col">
+              <span class="d-block">Any content related to the Soup Nazi is the property of the TV series, <a href="http://www.seinfeldscripts.com/TheSoupNazi.htm" target="_blank" title="Go to Seinfeld Soup Nazi transcript page">Seinfeld</a>. Please don't sue me.</span>
+              <span>&copy;
+                    <?php
+                        $startYear = 2018;
+                        $thisYear = date('Y');
+                        if ($startYear == $thisYear) {
+                            print $startYear;
+                        } else {
+                            print "{$startYear}&#8211;{$thisYear}";
+                        }
+                    ?> Design/Development by Vila Wong </span>
+            </div>
+          </div>
+ 
+        
+      </footer>
+  
+      <!-- Bootstrap core JavaScript
+      ================================================== -->
+      <!-- Placed at the end of the document so the pages load faster -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      
+      <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+      
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+      <script src="js/index.js"></script>
+    </body>
+  </html>
---
+++ Application/index.php
@@ -0,0 +1,70 @@
+<?php
+
+/* Vila Wong
+ * The home page for site
+*/
+
+#set page title and include header file:
+define('TITLE', 'The Soup Place - Home');
+include('includes/header.php');
+?>
+
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron mb-0">
+            <div class="container p-0">
+              <div class="col-md-6">
+                <h1>Daily Special | Soup Special</h1>
+                <p>List of ingredients in daily soup special, ingredients</p>
+                <p><a class="btn btn-primary btn-lg" href="#" role="button">See Entire Menu &raquo;</a></p>
+              </div>
+            </div>
+            <div class="clear"></div>
+        </div>
+        
+        <!-- <div id="app">
+            {{ message }}
+        </div> -->
+
+        <div id="procedure" class="bg-light">
+          <div class="container py-5">
+            <!-- PROCEDURE SECTION -->
+            <div class="row">
+              <div class="col">
+                <h2>Ordering Procedure</h2>
+                <h5>Follow procedure, get soup.</h5>
+                <ol>
+                    <li>Refert tamen, quo modo.</li>
+                    <li>Philosophi autem in suis lectulis plerumque moriuntur.</li>
+                    <li>Sed venio ad inconstantiae crimen, ne saepius dicas me aberrare;</li>
+                    <li>Ex rebus enim timiditas, non ex vocabulis nascitur.</li>
+                    <li>Id quaeris, inquam, in quo, utrum respondero, verses te huc atque illuc necesse est.</li>
+                    <li>Audax negotium, dicerem impudens, nisi hoc institutum postea translatum ad philosophos nostros esset.</li>
+                  </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="rules" class="bg-dark text-white">
+          <div class="container py-5">
+            <div class="row">
+                <div class="col">
+                  <h2>Rules</h2>
+                  <h5>Don't follow rules, then NO SOUP FOR YOU!</h5>
+                  <ul>
+                      <li>Refert tamen, quo modo.</li>
+                      <li>Philosophi autem in suis lectulis plerumque moriuntur.</li>
+                      <li>Sed venio ad inconstantiae crimen, ne saepius dicas me aberrare;</li>
+                      <li>Ex rebus enim timiditas, non ex vocabulis nascitur.</li>
+                      <li>Id quaeris, inquam, in quo, utrum respondero, verses te huc atque illuc necesse est.</li>
+                      <li>Audax negotium, dicerem impudens, nisi hoc institutum postea translatum ad philosophos nostros esset.</li>
+                    </ul>
+                </div>
+            </div>
+            
+          </div>
+        </div>
+
+<?php
+    include('includes/footer.php');
+?>
