b01026f33be687859b4c2c3b1902d0aa5075254d
vw-works <vila.wong.works@gmail.com>
2018-11-04 07:26:07 -0800

    Starting to convert to php
    Need to learn vue for reals

---
+++ Application/includes/header.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>
+  	<?php
+		if (defined('TITLE')) {
+			print TITLE;
+		} else {
+			print 'The Soup Place';
+		}
+	?>
+  </title>
+
+    <!--shiv to ensure that all browsers will work with HTML5-->
+    <!--[if IE]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- Get Vue Framework -->
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+  </head>
+
+  <body>
+
+      <!-- NAVIGATION -->
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark nav-masthead">
+        <div class="container">
+          <h3 class="my-1">
+            <a class="masthead-brand" href="index.html">The Soup Place</a>
+          </h3>
+          <button class="navbar-toggler p-0" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+    
+          <div class="collapse navbar-collapse" id="navbars">
+            <ul class="navbar-nav ml-auto nav-masthead">
+              <li class="nav-item active">
+                <a class="nav-link px-0 mx-md-2" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle px-0 mx-md-2" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Menu</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                  <a class="dropdown-item" href="#">All Soups</a>
+                  <a class="dropdown-item" href="#">Vegetarian Options</a>
+                  <a class="dropdown-item" href="#">Extras</a>
+                </div>
+              </li>
+
+              <li class="nav-item">
+                  <a class="nav-link px-0 mx-md-2" href="#">Hours &amp; Location</a>
+                </li>
+            </ul>
+          
+          </div>
+        </div>
+      </nav>
+      <!-- END NAVIGATION -->
+  
+      <main role="main">
+  <!-- BEGIN CHANGEABLE CONTENT. -->
